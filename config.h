825ffb56d3c51f70b1217eacb96a1b82d0652c85
sasank <gssasank@gmail.com>
2021-12-31 22:08:13 +0530

    added emoji support (?)

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "MesloLGS NF:size=12" };
-static const char dmenufont[]       = "MesloLGS NF:size=12";
+static const char *fonts[]          = { "MesloLGS NF:size=11", "JoyPixels:pixelsize=11" };
+static const char dmenufont[]       = "MesloLGS NF:size=11", "JoyPixels:pixelsize=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
