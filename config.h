4f5e585b080e1c8614825631b3f701db7b27f87f
sasank <gssasank@gmail.com>
2022-01-02 18:36:58 +0530

    added new glyphs and font awesome support

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */      
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "MesloLGS NF:size=11", "JoyPixels:pixelsize=11" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11", "Font Awesome 5 Brands:size=11", "JoyPixels:pixelsize=11" }; // "JetBrainsMono Nerd Font:size=11", 
 static const char dmenufont[]       = "MesloLGS NF:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "2", "3", "4", "5", "6", "7", "8", "ﭮ" };
+static const char *tags[] = { "", "2", "3", "4", "5", "6", "7", "8", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
