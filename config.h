3dfdb600454d1b6f023d1c5048788521228441e3
sasank <gssasank@gmail.com>
2022-01-03 01:16:41 +0530

    Added screenshot support using flameshot

--- config.h
+++ config.h
@@ -61,7 +61,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *volcmd[]  = { "pavucontrol", NULL };
-
+static const char *sscmd[]   = { "flameshot", "gui", NULL };
 
 // This is for shiftview.c, which enables lateral movement of tags that are adjacent to each other.
 #include "shiftview.c"
@@ -72,8 +72,10 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_v,      spawn,          {.v = volcmd} },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+        { MODKEY,                       XK_v,      spawn,          {.v = volcmd} },
+        { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = sscmd} },
+        { 0,                            XK_Print,  spawn,          {.v = sscmd} },	
+        { MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
